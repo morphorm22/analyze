ae1703c9a8d5a62822181b77b0f84dc26bdb2b6b
morphorm22 <maguilo@morphorm.com>
2022-11-21 12:08:09 -0700

    adds unit tests file for interpolation-based immersed fem

---
+++ unit_tests/IfemTests.cpp
@@ -0,0 +1,22 @@
+/*
+ * IfemTests.cpp
+ *
+ *  Created on: Nov 21, 2022
+ */
+
+// c++ includes
+#include <vector>
+
+// trilinos includes
+#include "Teuchos_UnitTestHarness.hpp"
+
+namespace IfemTests
+{
+
+TEUCHOS_UNIT_TEST(PlatoAnalyzeUnitTests, ElastoPlasticity_NewtonRaphsonStoppingCriterion)
+{
+    std::vector<double> tVector;
+}
+
+}
+// namespace IfemTests
