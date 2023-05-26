b60a58350b889c08a34c0b74476e0cf3552299e7
morphorm22 <maguilo@morphorm.com>
2023-05-26 10:09:28 -0600

    renames files

---
+++ src/elliptic/mechanical/CriterionVolumeIntegral.cpp
@@ -0,0 +1,12 @@
+#include "elliptic/mechanical/CriterionVolumeIntegral_decl.hpp"
+
+#ifdef PLATOANALYZE_USE_EXPLICIT_INSTANTIATION
+
+#include "elliptic/mechanical/CriterionVolumeIntegral_def.hpp"
+
+#include "MechanicsElement.hpp"
+#include "elliptic/ExpInstMacros.hpp"
+
+PLATO_ELLIPTIC_EXP_INST_2(Plato::Elliptic::CriterionVolumeIntegral, Plato::MechanicsElement)
+
+#endif
